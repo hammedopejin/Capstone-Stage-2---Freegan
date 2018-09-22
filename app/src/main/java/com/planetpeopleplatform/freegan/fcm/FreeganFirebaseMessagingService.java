2c8c3abddf913d1266d7e4417d834cd527912f64
Hammed Opejin <hammedopejin@yahoo.com>
2018-09-22 16:34:07 -0700

    feat: Implement notification builder for messages

--- app/src/main/java/com/planetpeopleplatform/freegan/fcm/FreeganFirebaseMessagingService.java
+++ app/src/main/java/com/planetpeopleplatform/freegan/fcm/FreeganFirebaseMessagingService.java
@@ -1,11 +1,30 @@
 package com.planetpeopleplatform.freegan.fcm;
 
+import android.app.NotificationManager;
+import android.app.PendingIntent;
+import android.content.ContentValues;
+import android.content.Context;
+import android.content.Intent;
+import android.media.RingtoneManager;
+import android.net.Uri;
+import android.os.AsyncTask;
+import android.support.v4.app.NotificationCompat;
 import android.util.Log;
 
 import com.google.firebase.messaging.FirebaseMessagingService;
+import com.google.firebase.messaging.RemoteMessage;
+import com.planetpeopleplatform.freegan.R;
+import com.planetpeopleplatform.freegan.activity.MainActivity;
+
+import java.util.Map;
+
+import static com.planetpeopleplatform.freegan.utils.Constants.kMESSAGE;
+import static com.planetpeopleplatform.freegan.utils.Constants.kUSER;
 
 public class FreeganFirebaseMessagingService extends FirebaseMessagingService {
+
     private static final String TAG = FreeganFirebaseMessagingService.class.getSimpleName();
+    private static final int NOTIFICATION_MAX_CHARACTERS = 30;
 
     /**
      * Called if InstanceID token is updated. This may occur if the security of
@@ -22,7 +41,84 @@ public class FreeganFirebaseMessagingService extends FirebaseMessagingService {
         sendRegistrationToServer(token);
     }
 
-    private void sendRegistrationToServer(String token) {
 
+    private void sendRegistrationToServer(String token) {
+        // This method is blank, but if you were to build a server that stores users token
+        // information, this is where you'd send the token to the server.
     }
+
+
+    /**
+     * Called when message is received.
+     *
+     * @param remoteMessage Object representing the message received from Firebase Cloud Messaging
+     */
+    @Override
+    public void onMessageReceived(RemoteMessage remoteMessage) {
+        // There are two types of messages data messages and notification messages. Data messages
+        // are handled
+        // here in onMessageReceived whether the app is in the foreground or background. Data
+        // messages are the type
+        // traditionally used with FCM. Notification messages are only received here in
+        // onMessageReceived when the app
+        // is in the foreground. When the app is in the background an automatically generated
+        // notification is displayed.
+        // When the user taps on the notification they are returned to the app. Messages
+        // containing both notification
+        // and data payloads are treated as notification messages. The Firebase console always
+        // sends notification
+        // messages.
+
+        Log.d(TAG, "From: " + remoteMessage.getFrom());
+
+        // Check if message contains a data payload.
+
+        Map<String, String> data = remoteMessage.getData();
+
+        if (data.size() > 0) {
+            Log.d(TAG, "Message data payload: " + data);
+
+            // Send a notification that you got a new message
+            sendNotification(data);
+
+        }
+    }
+
+
+    /**
+     * Create and show a simple notification containing the received FCM message
+     *
+     * @param data Map which has the message data in it
+     */
+    private void sendNotification(Map<String, String> data) {
+        Intent intent = new Intent(this, MainActivity.class);
+        intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
+        // Create the pending intent to launch the activity
+        PendingIntent pendingIntent = PendingIntent.getActivity(this, 0 /* Request code */, intent,
+                PendingIntent.FLAG_ONE_SHOT);
+
+        String user = data.get(kUSER);
+        String message = data.get(kMESSAGE);
+
+        // If the message is longer than the max number of characters we want in our
+        // notification, truncate it and add the unicode character for ellipsis
+        if (message.length() > NOTIFICATION_MAX_CHARACTERS) {
+            message = message.substring(0, NOTIFICATION_MAX_CHARACTERS) + "\u2026";
+        }
+
+        Uri defaultSoundUri = RingtoneManager.getDefaultUri(RingtoneManager.TYPE_NOTIFICATION);
+            NotificationCompat.Builder notificationBuilder = new NotificationCompat.Builder(this)
+                .setSmallIcon(R.drawable.ic_freegan_foreground)
+                .setContentTitle(String.format(getString(R.string.notification_message), user))
+                .setContentText(message)
+                .setAutoCancel(true)
+                .setSound(defaultSoundUri)
+                .setContentIntent(pendingIntent);
+
+        NotificationManager notificationManager =
+                (NotificationManager) getSystemService(Context.NOTIFICATION_SERVICE);
+
+        notificationManager.notify(0 /* ID of notification */, notificationBuilder.build());
+    }
+
 }
