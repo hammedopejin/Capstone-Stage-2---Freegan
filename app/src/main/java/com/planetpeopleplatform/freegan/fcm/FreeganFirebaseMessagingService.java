583625d07d06ca4f3e35efbfbc084bf885c3df98
Hammed Opejin <hammedopejin@yahoo.com>
2018-09-21 20:20:31 -0700

    feat: Create FreeganFirebaseMessagingService class

---
+++ app/src/main/java/com/planetpeopleplatform/freegan/fcm/FreeganFirebaseMessagingService.java
@@ -0,0 +1,28 @@
+package com.planetpeopleplatform.freegan.fcm;
+
+import android.util.Log;
+
+import com.google.firebase.messaging.FirebaseMessagingService;
+
+public class FreeganFirebaseMessagingService extends FirebaseMessagingService {
+    private static final String TAG = FreeganFirebaseMessagingService.class.getSimpleName();
+
+    /**
+     * Called if InstanceID token is updated. This may occur if the security of
+     * the previous token had been compromised. Note that this is called when the InstanceID token
+     * is initially generated so this is where you would retrieve the token.
+     */
+    @Override
+    public void onNewToken(String token) {
+        Log.d(TAG, "Refreshed token: " + token);
+
+        // If you want to send messages to this application instance or
+        // manage this apps subscriptions on the server side, send the
+        // Instance ID token to your app server.
+        sendRegistrationToServer(token);
+    }
+
+    private void sendRegistrationToServer(String token) {
+
+    }
+}
--- app/src/main/java/com/planetpeopleplatform/freegan/utils/PushNotifications.java
+++ app/src/main/java/com/planetpeopleplatform/freegan/utils/PushNotifications.java
@@ -1,25 +1,31 @@
 package com.planetpeopleplatform.freegan.utils;
 
 import android.support.annotation.NonNull;
+import android.util.Log;
 
 import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.ValueEventListener;
+import com.planetpeopleplatform.freegan.model.User;
 
+import java.util.ArrayList;
 import java.util.Collection;
 import java.util.HashMap;
 
 import static com.planetpeopleplatform.freegan.utils.Constants.firebase;
 import static com.planetpeopleplatform.freegan.utils.Constants.kCHATROOMID;
+import static com.planetpeopleplatform.freegan.utils.Constants.kCOUNTER;
 import static com.planetpeopleplatform.freegan.utils.Constants.kMEMBERS;
 import static com.planetpeopleplatform.freegan.utils.Constants.kRECENT;
+import static com.planetpeopleplatform.freegan.utils.Constants.kUSERID;
 
 public class PushNotifications {
+
     private static DatabaseReference mRef = firebase.child(kRECENT);
     private static boolean mShouldSendPushNotification = false;
 
-    public  static void sendPushNotification1(String chatRoomID, final String message) {
+    public  static void sendPushNotification1(String chatRoomID, final String message, final String currentUserId) {
 
         mRef.orderByChild(kCHATROOMID).equalTo(chatRoomID).addListenerForSingleValueEvent(new ValueEventListener() {
             @Override
@@ -30,7 +36,8 @@ public class PushNotifications {
                     for (Object recent : recents) {
 
                         final HashMap<String, Object> currentRecent = (HashMap<String, Object>) recent;
-                        sendPushNotification2((String) currentRecent.get(kMEMBERS), message);
+                        sendPushNotification2((String[]) currentRecent.get(kMEMBERS), message, currentUserId);
+                        //String[] list = (String[]) currentRecent.get(kMEMBERS);
                     }
                 }
             }
@@ -43,8 +50,107 @@ public class PushNotifications {
 
     }
 
-    private static void sendPushNotification2(String s, String message) {
+    private static void sendPushNotification2(String[] members, String message, String currentUserId) {
 
+        String[] newMembers = removeCurrentUserFromMembersArray(members, currentUserId);
+
+        User[] usersArray = getMembersToPush(newMembers);
+
+            for (User user : usersArray) {
+                mShouldSendPushNotification = true;
+
+                sendPushNotification(user, message);
+            }
+
+    }
+
+    private static void sendPushNotification(User user, String message) {
+        int counter = numberOfUnreadMessagesOfUser(user.getObjectId());
+
+            //String deviceId = (String) user.getProperty(kDEVICEID);
+
+            //DeliveryOptions deliveryOptions = new DeliveryOptions();
+
+            //deliveryOptions.pushSinglecast = deviceId[];
+
+
+            //PublishOptions publishOptions = PublishOptions();
+
+           // publishOptions.assignHeaders(["\(firebase.userService.currentUser.name!) \n \(message)",
+            //     "\(counter)",  "default"]);
+
+//            if (firebase.messaging.publish("default", message,  publishOptions, deliveryOptions)){
+//
+//                mShouldSendPushNotification = false;
+//
+//            }else{
+//
+//                Log.d("TAG", "sendPushNotification: ");
+//            }
+    }
+
+    private static int numberOfUnreadMessagesOfUser(String userId) {
+
+
+        final int[] counter = {0};
+        final int[] resultCounter = {0};
+
+        mRef.orderByChild(kUSERID).equalTo(userId).addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+                if (dataSnapshot.exists()) {
+
+                    Collection<Object> recents =
+                            ((HashMap<String, Object>) dataSnapshot.getValue()).values();
+
+                    for (Object recent : recents) {
+
+                        final HashMap<String, Object> currentRecent = (HashMap<String, Object>) recent;
+                        int tempCount = ((int) currentRecent.get(kCOUNTER));
+
+                        resultCounter[0] += 1;
+                        counter[0] += tempCount;
+
+//                        if (mShouldSendPushNotification) {
+//
+//                            if (resultCounter[0] == recents.size()) {
+//
+//                                return counter[0];
+//                            }
+//                        }
+
+                    }
+
+                }
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError databaseError) {
+
+            }
+        });
+
+        return 0;
+
+    }
+
+    private static User[] getMembersToPush(String[] newMembers) {
+        return null;
+    }
+
+    private static String[] removeCurrentUserFromMembersArray(String[] members, String currentUserId) {
+
+        String[] updatedMembers = new String[1];
+
+        for (String member : members) {
+
+            if (!member.equals(currentUserId)) {
+
+                updatedMembers[0] = (member);
+            }
+        }
+
+        return updatedMembers;
     }
 
 
